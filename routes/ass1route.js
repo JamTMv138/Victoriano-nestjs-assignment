22a3111664e93cc39c4208fc817adb7e7e1255d0
JamTMv138 <jamv138@gmail.com>
2025-01-04 22:44:08 +0800

    deleted
    removed duplicates

--- routes/ass1route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateFibonacci } = require('../controller/ass1controller');  // Corrected path
-
-router.get('/fibonacci/:num', generateFibonacci);
-
-module.exports = router;
