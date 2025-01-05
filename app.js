181f6203ad2523cc8945a5c81ea5423b9be2c5a0
JamTMv138 <jamv138@gmail.com>
2025-01-05 10:36:59 +0800

    deleted duplicate

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const fibonacciRoutes = require('./routes/ass1route');  // Corrected path
-
-app.use('/api', fibonacciRoutes);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
